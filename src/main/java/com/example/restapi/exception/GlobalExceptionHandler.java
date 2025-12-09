2a47a28ba790ff9f90d4786448b7f4550143a54b
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-12-09 09:37:06 +0900

    JWT 설정

--- src/main/java/com/example/restapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/restapi/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.example.restapi.exception;
 
 import com.example.restapi.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,6 +21,18 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.error(errorCode.getCode(),errorCode.getMessage()));
     }
     // Validation 예외 처리
-    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.badRequest().body(ApiResponse.error("VALIDATION_ERROR", message));
+    }
     // 그 외 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(Exception ex){
+        return ResponseEntity.internalServerError()
+                .body(ApiResponse.error("INTERNAL SERVER ERROR","서버 오류가 발생했습니다."));
+    }
 }
