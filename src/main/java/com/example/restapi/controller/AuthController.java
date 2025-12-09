06862fb8ec406ebc6d41bf3db0aa4a89ad32ae76
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-12-09 14:20:36 +0900

    SignUp, Login 기능구현을 위한 Controller 작성

---
+++ src/main/java/com/example/restapi/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.dto.request.LoginRequest;
+import com.example.restapi.dto.request.SignupRequest;
+import com.example.restapi.dto.response.ApiResponse;
+import com.example.restapi.dto.response.TokenResponse;
+import com.example.restapi.dto.response.UserResponse;
+import com.example.restapi.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<UserResponse>> signup(
+            @Valid @RequestBody SignupRequest request
+    ){
+        UserResponse response = authService.signUp(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<TokenResponse>> login(
+            @Valid @RequestBody LoginRequest request
+    ){
+        TokenResponse response = authService.login(request);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+}
